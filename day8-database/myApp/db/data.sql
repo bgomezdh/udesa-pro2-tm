3950a1f23c1fc4153d5e8d80fb7c7a305dae380e
bgomezdh <bgomez@digitalhouse.com>
2022-05-05 08:51:34 -0300

    Modifique el orden de sql

--- day8-database/myApp/db/data.sql
+++ day8-database/myApp/db/data.sql
@@ -1,19 +1,21 @@
 USE catalogo;
 
-CREATE TABLE usuario (
+/* Creamos tabla usuarios */
+CREATE TABLE usuarios (
 /*	nombreColumna		tipoDato		Restricciones*/
-	id	INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nombre 			VARCHAR(50)			NOT NULL,
-    apellido 		VARCHAR(50)			NOT NULL,
-    email 			VARCHAR(50)			NOT NULL,
-    usuario 		VARCHAR(50)			NOT NULL,
-    constrasenia 	VARCHAR(50)			NOT NULL,
+	  id	INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre 			    VARCHAR(50)			NOT NULL,
+    apellido 		    VARCHAR(50)			NOT NULL,
+    email 			    VARCHAR(50)			NOT NULL,
+    usuario 		    VARCHAR(50)			NOT NULL,
+    constrasenia 	  VARCHAR(50)			NOT NULL,
     fechaNacimiento	DATE 				NOT NULL,
     numeroDocumento	INT 				NOT NULL,
-    foto			VARCHAR(300)		
+    foto			      VARCHAR(300)		
 );
 
-CREATE TABLE `catalogo`.`producto` (
+/* Creamos tabla productos */
+CREATE TABLE `catalogo`.`productos` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `img` VARCHAR(45) NULL,
   `nombre_producto` VARCHAR(100) NULL,
